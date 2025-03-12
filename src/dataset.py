5ccf624c5082846af20678ee7fa4608f9c624fde
Allard-code-web <allardv03@gmail.com>
2025-03-12 11:08:20 +0100

    update

---
+++ src/dataset.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+data = {
+    "name": ["John", "Anna", "Peter", "Linda"],
+    "age": [23, 36, 45, 33],
+    "city": ["New York", "Paris", "Berlin", "London"]
+ }
+
+df = pd.DataFrame(data)
+
