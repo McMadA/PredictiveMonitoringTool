568530095345bc7c4ab94f296bdf5f548ae59e24
Allard-code-web <allardv03@gmail.com>
2025-04-15 22:56:24 +0200

    en

--- src/best-window.py
+++ src/best-window.py
@@ -79,7 +79,7 @@ for window_size in range(5, 55, 5):
     y = np.array(y)
     
     # Split the data into training and testing sets
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=69)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
     
     # Scale the features
     scaler = StandardScaler()
