708f3a6c01603e6ecb16c614f13ba6fc4144c89d
Broer Allard <allardv03@gmail.com>
2025-04-17 16:01:40 +0200

    comment

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ except Exception as e:
 else:
     models_loaded = True
 
-Define window size as in your original code
+#Define window size as in your original code
 window_size = 10
 
 @app.route('/')
